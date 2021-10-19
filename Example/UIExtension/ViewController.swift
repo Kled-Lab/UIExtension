1d14e9525e41776b08131ada3ae04114cba672f2
xxxx <xxxx.xxx@xxx.com>
2021-10-19 16:25:41 +0800

    Initial commit

---
+++ Example/UIExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIExtension
+//
+//  Created by kled on 10/19/2021.
+//  Copyright (c) 2021 kled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
